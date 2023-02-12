7e251935e86639927b91812b8e05be62b972e940
daniel Kabagambe <danielkabagambe@gmail.com>
2023-02-12 18:23:49 +0300

    working on php

--- eligibility.php
+++ eligibility.php
@@ -1,7 +1,10 @@
-<?php
 
-if(isset($_POST["submit"])){
-    $business_period = $_POST["business-period"];
+<?php 
+if(isset($_POST['submit'])){
+    $to = 'info@hebronfinance.com'; // this is your Email address
+    $from = $_POST['email-address']; // this is the sender's Email address
+ 
+     $business_period = $_POST["business-period"];
     $loan_amount = $_POST["loan-amount"];
     $first_name = $_POST["first-name"];
     $last_name = $_POST["last-name"];
@@ -24,39 +27,34 @@ if(isset($_POST["submit"])){
     $industry = $_POST["industry"];
     $number_of_employees = $_POST["number-of-employees"];
 
-    $to = "info@hebronfinance.com";
-    $subject = "Business Loan Request";
+ $subject = "Business Loan Request";
     $message = "A user has submitted a business loan request with the following details:
     
     Business Period: $business_period
     Loan Amount: $loan_amount
     First Name: $first_name
-    Last Name: $last_name
-    Email Address: $email_address
-    Phone Number: $phone_number
-    Job Role: $job_role
-    How did you hear about Hebron Finance?: $hearing_source
-    Company Name: $cname
-    Business Type: $business_type
-    Business Tax ID: $business_tax_id
-    Business Established: $business_established
-    Business Address: $business_address
-    Business Address1: $business_address1
-    Business City: $business_city
-    Business State: $business_state
-    Business Zip: $business_zip
-    Annual Revenue: $annual_revenue
-    Time in Business: $time_in_business
-    Industry: $industry
-    Number of Employees: $number_of_employees
-    ";
-    $headers = "From: $email_address";
-
-    if(mail($to, $subject, $message, $headers)){
-        echo "The email was sent successfully!";
-    }else{
-        echo "There was an error sending the email.";
+     Last Name: $last_name
+     Email Address: $email_address
+     Phone Number: $phone_number
+     Job Role: $job_role
+     How did you hear about Hebron Finance?: $hearing_source
+     Company Name: $cname
+     Business Type: $business_type
+     Business Tax ID: $business_tax_id
+     Business Established: $business_established
+     Business Address: $business_address
+     Business Address1: $business_address1
+     Business City: $business_city
+     Business State: $business_state
+     Business Zip: $business_zip
+     Annual Revenue: $annual_revenue
+     Time in Business: $time_in_business
+     Industry: $industry
+     Number of Employees: $number_of_employees
+     ";
+    $headers = 'From:' . $from;
+    mail($to,$subject,$message,$headers);
+    echo 'Mail Sent. Thank you for showing interest in hebron finance' . $first_name . ', we will contact you shortly.';
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
     }
-}
-
 ?>
